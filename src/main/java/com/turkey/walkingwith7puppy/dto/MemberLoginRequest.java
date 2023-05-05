4902cca9ad24b08736d4634ac5a1016f7f0b71c9
우께몬\kimcl <kimjaehyeong567@gmail.com>
2023-05-05 22:57:08 +0900

    시큐리티,로그인,회원가입

---
+++ src/main/java/com/turkey/walkingwith7puppy/dto/MemberLoginRequest.java
@@ -0,0 +1,14 @@
+package com.turkey.walkingwith7puppy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class MemberLoginRequest {
+
+
+
+    private String username;
+
+    private String password;
+}
--- src/main/java/com/turkey/walkingwith7puppy/entity/Member.java
+++ src/main/java/com/turkey/walkingwith7puppy/entity/Member.java
@@ -14,6 +14,9 @@ public class Member {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false,unique = true)
+  private String username;
+
   @Column(nullable = false)
   private String password;
 
