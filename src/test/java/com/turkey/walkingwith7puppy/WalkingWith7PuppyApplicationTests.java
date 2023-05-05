ab7e1f2fbae8e09c90fafd852dd5d9c7070b44bf
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-05-05 11:38:25 +0900

    master : 초기버전

---
+++ src/test/java/com/turkey/walkingwith7puppy/WalkingWith7PuppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkey.walkingwith7puppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingWith7PuppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
