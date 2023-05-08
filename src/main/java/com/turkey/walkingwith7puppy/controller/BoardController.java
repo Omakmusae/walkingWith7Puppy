ffb6b57a4b9e9fb4b5b198b24f34f046513b63b4
Cho-woo-pil <jwp4465@naver.com>
2023-05-08 17:57:57 +0900

    fix : aws key 노출로 인한 암호화키 변경

--- src/main/java/com/turkey/walkingwith7puppy/controller/BoardController.java
+++ src/main/java/com/turkey/walkingwith7puppy/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.turkey.walkingwith7puppy.controller;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -28,50 +29,52 @@ public class BoardController {
 	public ResponseEntity<List<BoardResponse>> getBoardList() {
 
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(boardService.searchBoards());
+				.body(boardService.searchBoards());
 	}
 
 	@GetMapping("/boards/{boardId}")
 	public ResponseEntity<BoardResponse> getBoard(@PathVariable final Long boardId) {
 
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(boardService.searchBoard(boardId));
+				.body(boardService.searchBoard(boardId));
 	}
 
 	@PostMapping("/boards")
 	public ResponseEntity<Void> createBoard(
+
 		@AuthenticationPrincipal final UserDetailsImpl userDetails,
 		@RequestPart(value = "data") @Valid final BoardRequest boardRequest,
-		@RequestPart(value = "imgUrl") final MultipartFile file) {
-    
+		@RequestPart(value = "ImgUrl") MultipartFile file) {
 		boardService.createBoard(userDetails.getMember(), boardRequest, file);
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 
 	@PutMapping("/boards/{boardId}")
 	public ResponseEntity<Void> updateBoard(
+
 		@AuthenticationPrincipal final UserDetailsImpl userDetails,
 		@PathVariable final Long boardId,
 		@RequestPart(value = "data") @Valid final BoardRequest boardRequest,
-		@RequestPart(value = "imgUrl") final MultipartFile file) {
-
+		@RequestPart(value = "ImgUrl") MultipartFile file) {
 		boardService.updateBoard(userDetails.getMember(), boardId, boardRequest, file);
+
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 
 	@DeleteMapping("/boards/{boardId}")
 	public ResponseEntity<Void> deleteArticle(
-		@AuthenticationPrincipal final UserDetailsImpl userDetails,
-		@PathVariable final Long boardId) {
+			@AuthenticationPrincipal final UserDetailsImpl userDetails,
+			@PathVariable final Long boardId) {
 
 		boardService.deleteBoard(userDetails.getMember(), boardId);
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 
-	@GetMapping("/boards/search")
-	public ResponseEntity<List<BoardResponse>> getByAddressBoardList(@RequestParam String address) {
-
+	@GetMapping("/boards/123")
+	public ResponseEntity<List<BoardResponse>> searchBoardList(@RequestParam String address) {
+		System.out.println(address);
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(boardService.searchBoards(address));
+				.body(boardService.searchBoards(address));
 	}
+
 }
