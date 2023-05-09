7fdf503cef7393447441952f1ab056f9e9901e6c
kimgun95 <harrison1995@naver.com>
2023-05-10 01:41:43 +0900

    feat: Max 유효성 검증 삭제 및 Member 서비스 로직 에러 핸들

--- src/main/java/com/turkey/walkingwith7puppy/dto/request/BoardRequest.java
+++ src/main/java/com/turkey/walkingwith7puppy/dto/request/BoardRequest.java
@@ -1,6 +1,5 @@
 package com.turkey.walkingwith7puppy.dto.request;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
@@ -9,11 +8,9 @@ import lombok.Getter;
 public class BoardRequest {
 
 	@NotNull
-	@Max(value = 20)
 	private String title;
 
 	@NotNull
-	@Max(value = 50)
 	private String content;
 
 	@NotNull
