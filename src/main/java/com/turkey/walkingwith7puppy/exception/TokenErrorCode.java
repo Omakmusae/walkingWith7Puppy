eecfe3a6c4f5c22aac3de05054a9cad407df44e7
kimgun95 <harrison1995@naver.com>
2023-05-10 10:03:25 +0900

    feat: TokenErrorCode 수정

--- src/main/java/com/turkey/walkingwith7puppy/exception/TokenErrorCode.java
+++ src/main/java/com/turkey/walkingwith7puppy/exception/TokenErrorCode.java
@@ -12,7 +12,9 @@ public enum TokenErrorCode implements ErrorCode {
 	EXPIRED_TOKEN (HttpStatus.BAD_REQUEST, "만료된 JWT token 입니다"),
 	INVALID_TOKEN (HttpStatus.BAD_REQUEST, "유효하지 않는 JWT 서명 입니다"),
 	UNSUPPORTED_TOKEN (HttpStatus.BAD_REQUEST, "지원되지 않는 JWT 토큰 입니다"),
-	EMPTY_TOKEN (HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰 입니다")
+	EMPTY_TOKEN (HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰 입니다"),
+	EXPIRED_ACCESS_TOKEN (HttpStatus.BAD_REQUEST, "만료된 Access token 입니다"),
+	EXPIRED_REFRESH_TOKEN (HttpStatus.BAD_REQUEST, "만료된 Refresh token 입니다"),
 	;
 
 	private final HttpStatus httpStatus;
